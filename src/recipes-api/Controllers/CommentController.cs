bd96f0b2425618e2d5657d9fcc46f765e90d78cb
nickolasvm <nickolas64vm@gmail.com>
2023-11-17 19:28:14 -0300

    feat: implemented the comments controller methods, to create a new comment and return a list of comments based on the recipes name

--- src/recipes-api/Controllers/CommentController.cs
+++ src/recipes-api/Controllers/CommentController.cs
@@ -12,25 +12,27 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("comment")]
 public class CommentController : ControllerBase
-{  
+{
     public readonly ICommentService _service;
-    
+
     public CommentController(ICommentService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 10 - Sua aplicação deve ter o endpoint POST /comment
     [HttpPost]
-    public IActionResult Create([FromBody]Comment comment)
+    public IActionResult Create([FromBody] Comment comment)
     {
-        throw new NotImplementedException();
+        _service.AddComment(comment);
+        return CreatedAtAction("Get", new { name = comment.RecipeName }, comment);
     }
 
     // 11 - Sua aplicação deve ter o endpoint GET /comment/:recipeName
     [HttpGet("{name}", Name = "GetComment")]
     public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();                   
+    {
+        List<Comment> comments = _service.GetComments(name);
+        return Ok(comments);
     }
 }
