ef433d6ba6802c5d0ffed0be07787704f27c08e5
nickolasvm <nickolas64vm@gmail.com>
2023-11-17 18:20:32 -0300

    feat: Implemented PUT request to update recipes

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -50,7 +50,15 @@ public class RecipesController : ControllerBase
     [HttpPut("{name}")]
     public IActionResult Update(string name, [FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _service.UpdateRecipe(recipe);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 
     // 5 - Sua aplicação deve ter o endpoint DEL /recipe
